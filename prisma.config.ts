89521a249ceda4b463bce24d4dda5bc22f1063a4
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2026-06-16 16:08:40 +0200

    Add mock data seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,10 +5,11 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
   },
   datasource: {
     url:
       process.env.DATABASE_URL ??
-      "postgres://michelin:michelin@localhost:5432/michelin",
+      "postgres://michelin:michelin@localhost:55432/michelin",
   },
 });
